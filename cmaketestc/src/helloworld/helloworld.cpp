c2616d4ab3ef927313ee03f5d94d386b21e1cd3a
Zhongming Qu <qzmfrank@umich.edu>
2014-02-24 10:37:31 -0500

    deleted unused dirs

--- cmaketestc/src/helloworld/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "helloworld.h"
-/******************************************************************************/
-int main(int argc, char const* argv[])
-{
-	using namespace std;
-	cout << "hello, world!" << endl;
-	return 0;
-}
